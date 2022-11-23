2db210b25859a1d3302ed7c9ff3c8c88640abcbf
kisnak21 <krisnastya21@gmail.com>
2022-11-23 11:41:56 +0700

    add write post component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import DetailPost from "./pages/detail/DetailPost";
+import WritePost from "./pages/writepost/WritePost";
 
 function App() {
   return (
     <>
     <TopBar />
-    <DetailPost />
+    <WritePost />
     </>
   );
 }
