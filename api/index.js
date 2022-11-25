89ce6f5bfa0d0c23026c7570e639684fdfe4d588
kisnak21 <krisnastya21@gmail.com>
2022-11-25 16:07:14 +0700

    add CRUD posts

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
 
 dotenv.config();
 app.use(express.json());
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_DB).then(console.log('connected to mongodb'))
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 
 app.listen('5000', () => {
   console.log('backend is running');
