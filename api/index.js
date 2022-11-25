04471c42f7f61e1a585b5ae2e1d26f0ee6f22fe0
kisnak21 <krisnastya21@gmail.com>
2022-11-25 10:19:09 +0700

    add register and login authentication

--- api/index.js
+++ api/index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL).then(console.log('connected to mongodb')).catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_DB).then(console.log('connected to mongodb')).catch((err) => { console.log(err); });
+
+app.use('/api/auth', authRoute);
 
 app.listen('5000', () => {
   console.log('backend is running');
